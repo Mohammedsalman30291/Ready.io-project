e1a86ffd611ff24d66272c975264c2c62253d1e7
Mohammed Salman <mohammedsalman30291@gmail.com>
2025-12-29 22:32:53 +0530

    ready to submit

--- readyiopro/src/pages/Home.tsx
+++ readyiopro/src/pages/Home.tsx
@@ -2,12 +2,31 @@ import { Link } from "react-router-dom"
 
 export default function Home() {
   return (
-    <div className="page">
-      <h1>Welcome to FakeStore </h1>
-      <p>This is application was build by Mohammed Salman for the interview task</p>
-      <Link to="/products" className="btn">
-        View Products
-      </Link>
+    <div className="home">
+      <div className="home-content fade-in">
+        <h1 className="slide-up">Welcome to FakeStore SPA</h1>
+
+        <p className="slide-up delay-1">
+          This is a Single Page Application built using React and TypeScript.
+          It fetches real-time product data from a public API and displays it
+          using dynamic routing and reusable components done by <b>Mohammed Salman H.</b>
+        </p>
+
+        <div className="home-actions slide-up delay-2">
+          <Link to="/products" className="btn">
+            Explore Products
+          </Link>
+
+          <a
+            href="https://fakestoreapi.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="btn btn-outline"
+          >
+            View API
+          </a>
+        </div>
+      </div>
     </div>
   )
 }
